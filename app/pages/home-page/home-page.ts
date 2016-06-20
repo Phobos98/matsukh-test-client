b00856c3ba129487f80fb0bb25778cebc889582e
Ivan Kourza <phobos98@phobos98.net>
2016-06-20 15:04:21 +0300

    Some fixes

--- app/pages/home-page/home-page.ts
+++ app/pages/home-page/home-page.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {NavController, Platform, Loading, Alert} from 'ionic-angular';
+import {NavController, Loading, Alert} from 'ionic-angular';
 import {Network, Connection} from 'ionic-native';
 
 import {HttpClient} from '../../services/http';
@@ -9,44 +9,60 @@ import {HttpClient} from '../../services/http';
 })
 export class HomePage {
 
-  private _connected:boolean = true;
+  private _connected:boolean = false;
   private _serverAvailable:boolean = false;
 
   private _messages:Array<any> = [];
 
-  constructor(private _nav: NavController, private _http: HttpClient, private platform: Platform) {
+  constructor(private _nav: NavController, private _http: HttpClient) {
 
     let connectionLoading = Loading.create({
-      content: 'Перевірка підключення...'
+      content: 'Очікування підключення...'
     });
 
     this._nav.present(connectionLoading);
 
-    platform.ready().then(() => {
-      console.warn('platform.ready');
-      console.warn(Network.connection, Connection.WIFI);
+    let connectSubscription = Network.onConnect().subscribe(() => {
       connectionLoading.dismiss();
-      this._loadMessages();
-      let connectSubscription = Network.onConnect().subscribe(() => {
-        console.warn('network connected');
-        if (Network.connection === Connection.WIFI) {
-          console.warn('connection is wifi');
-          this._connected = true;
-          this._loadMessages();
-        }
-      });
+      connectionLoading.dismiss();
+      console.warn('network connected');
+      if (Network.connection === Connection.WIFI) {
+        console.warn('connection is wifi');
+        this._connected = true;
+        this._loadMessages();
+      }
     });
 
   }
 
-  _loadMessages() {
+  refresh(refresher:any) {
+    this._loadMessages(refresher);
+  }
+
+  _loadMessages(refresher:any = null) {
+    //this._connected = (Network.connection === Connection.WIFI);
     let connectionLoading = Loading.create({
       content: 'Підключення до сервера...'
     });
     this._nav.present(connectionLoading);
     this._http.get('/messages', null).subscribe(
-      (data) => { connectionLoading.dismiss().then( () => { this._serverAvailable = true; this._messages = data.data.items } ); },
-      (err) => { connectionLoading.dismiss().then( () => { this._serverAvailable = false; } ); }
+      (data) => {
+        connectionLoading.dismiss().then(() => {
+          this._serverAvailable = true;
+          this._messages = data.data.items;
+          if (refresher) {
+            refresher.complete();
+          }
+        } );
+      },
+      (err) => {
+        connectionLoading.dismiss().then(() => {
+          this._serverAvailable = false;
+          if (refresher) {
+            refresher.complete();
+          }
+        } );
+      }
     );
   }
 
